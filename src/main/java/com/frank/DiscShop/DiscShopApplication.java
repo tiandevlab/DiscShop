93b3e5cf7ce8613d2b15310adae76ff5706d7949
Tianyi.Huang <fh.frankhuang@gmail.com>
2024-04-03 15:23:52 +0100

    Initial from Spring Initializr

---
+++ src/main/java/com/frank/DiscShop/DiscShopApplication.java
@@ -0,0 +1,13 @@
+package com.frank.DiscShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/frank/DiscShop/DiscShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frank.DiscShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
